214d7c3c0d36f79974866275dd6abc87142c79f2
Stefan CSE <stefankeyboard2002@gmail.com>
2026-03-02 20:01:56 +0100

    Agregue documentacion Swagger

---
+++ api/docs.js
@@ -0,0 +1,15 @@
+// Endpoint para la documentacion Swagger
+// Accede a /api/docs para ver la documentacion interactiva
+// Se accede mediante /api/docs para ver la documentacion interactiva
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
+const swaggerDocument = YAML.load(path.join(process.cwd(), 'swagger.yaml'));
+
+export default function handler(req, res) {
+  // Configuracion de Swagger UI
+  const html = swaggerUi.generateHTML(swaggerDocument);
+  res.send(html);
+}
