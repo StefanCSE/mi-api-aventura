545d3ef186d2fbdb116eca12e3dbd91680e1e2a9
Stefan CSE <stefankeyboard2002@gmail.com>
2026-03-02 20:19:29 +0100

    try 4

--- api/docs.js
+++ api/docs.js
@@ -7,9 +7,18 @@ const YAML = require('yamljs');
 const path = require('path');
 
 const swaggerDocument = YAML.load(path.join(process.cwd(), 'swagger.yaml'));
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css";
 
 export default function handler(req, res) {
-  // Configuracion de Swagger UI
-  const html = swaggerUi.generateHTML(swaggerDocument);
-  res.send(html);
+  try {
+    // Generamos el HTML pasando el link del CSS externo
+    const html = swaggerUi.generateHTML(swaggerDocument, {
+      customCssUrl: CSS_URL
+    });
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send(html);
+  } catch (error) {
+    res.status(500).json({ error: "Error renderizando docs", details: error.message });
+  }
 }
